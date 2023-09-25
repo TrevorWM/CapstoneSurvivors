5972e637a35736914bafbea82241f2c20271edca
Trevor <trevor.meest@gmail.com>
2023-09-25 15:11:20 -0600

    Removed imports that were causing build fails

--- Assets/Scripts/Combat/Common/DamageCalculator.cs
+++ Assets/Scripts/Combat/Common/DamageCalculator.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Rendering.FilterWindow;
+
 
 public class DamageCalculator : MonoBehaviour
 {
