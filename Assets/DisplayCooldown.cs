87aac86c7dbd1b222122d6586dc66ab2a4569325
Trevor <trevor.meest@gmail.com>
2023-09-29 16:18:14 -0600

    Cooldowns show on hotbar

---
+++ Assets/DisplayCooldown.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayCooldown : MonoBehaviour
+{
+    [SerializeField]
+    private Slider cooldownBar;
+
+    private void Start()
+    {
+        cooldownBar = GetComponent<Slider>();
+    }
+
+    public void StartShowCooldown(float duration)
+    {
+        StartCoroutine(ShowCooldown(duration));
+    }
+
+    private IEnumerator ShowCooldown(float duration)
+    {
+        float timeSinceAbilityStart = 0f;
+
+        while (timeSinceAbilityStart < duration)
+        {
+            timeSinceAbilityStart += Time.deltaTime;
+            if (cooldownBar) cooldownBar.value = Mathf.Lerp(0f, 1f, timeSinceAbilityStart / duration);
+            yield return null;
+        }      
+    }
+}
