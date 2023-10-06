59a344755467557f052da3fe98801e7fd741fb5f
Zachary Swensrude <swensrudez@gmail.com>
2023-10-06 16:13:05 -0600

    pause menu now shows all current stats

---
+++ Assets/Scripts/Player/PauseInfo.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PauseInfo : MonoBehaviour
+{
+    private CharacterStats playerStats;
+
+    [SerializeField]
+    private GameObject damageText;
+    [SerializeField]
+    private GameObject rechargeText;
+    [SerializeField]
+    private GameObject defenseText;
+    [SerializeField]
+    private GameObject speedText;
+    [SerializeField]
+    private GameObject fireText;
+    [SerializeField]
+    private GameObject waterText;
+    [SerializeField]
+    private GameObject natureText;
+    [SerializeField]
+    private GameObject critDamText;
+    [SerializeField]
+    private GameObject critRateText;
+
+    private void OnEnable()
+    {
+        playerStats = GetComponentInParent<CharacterStats>();
+        playerStats.PrintStatSheet();
+        DisplayStats();
+    }
+
+    private void DisplayStats()
+    {
+        damageText.GetComponent<TextMeshProUGUI>().text = "Base Damage: " + playerStats.BaseDamage + " damage";
+
+        rechargeText.GetComponent<TextMeshProUGUI>().text = "Cooldown Reduction: " + playerStats.CooldownReduction * 100f +"% faster recharge and attack rate";
+
+        defenseText.GetComponent<TextMeshProUGUI>().text = "Defense: Reduce all damage taken by " + playerStats.Defense + " damage";
+
+        speedText.GetComponent<TextMeshProUGUI>().text = "Speed: ";// + playerStats.Speed + "% increase";
+
+        fireText.GetComponent<TextMeshProUGUI>().text = "Fire Affinity: Increase all Fire damage by " + playerStats.FireAffinity * 100f + "%";
+
+        waterText.GetComponent<TextMeshProUGUI>().text = "Water Affinity: Increase all Water damage by " + playerStats.WaterAffinity * 100f + "%";
+
+        natureText.GetComponent<TextMeshProUGUI>().text = "Nature Affinity: Increase all Nature damage by " + playerStats.NatureAffinity * 100f + "%";
+
+        critDamText.GetComponent<TextMeshProUGUI>().text = "Critical Damage Multiplier: " + playerStats.CriticalDamageMultiplier * 100f + "% base damage";
+
+        critRateText.GetComponent<TextMeshProUGUI>().text = "Critical Damage Chance: " + playerStats.CriticalChance + "%";
+    }
+
+}
