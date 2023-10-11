6c1dd57fa1f3ce409c4667e94cf2f40c575778d6
Trevor <trevor.meest@gmail.com>
2023-10-11 15:10:18 -0600

    Starting boss logic

---
+++ Assets/Scripts/Combat/Enemies/Bosses/Demon Door/DemonDoor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonDoor : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterStatsSO bossStats;
+
+    [SerializeField]
+    private Transform targetPosition;
+
+    [SerializeField]
+    private Transform[] bossShootPositions;
+
+
+}
