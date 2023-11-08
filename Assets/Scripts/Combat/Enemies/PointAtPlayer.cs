29feebe39987c9606e1225862269f7044cea3197
Zachary Swensrude <swensrudez@gmail.com>
2023-11-07 19:51:24 -0700

    add angleOffset to pointAtPlayer as SF

--- Assets/Scripts/Combat/Enemies/PointAtPlayer.cs
+++ Assets/Scripts/Combat/Enemies/PointAtPlayer.cs
@@ -6,6 +6,8 @@ public class PointAtPlayer : MonoBehaviour
 {
     [SerializeField]
     private LayerMask hitLayers;
+    [SerializeField]
+    private float angleOffset = -45f;
     readonly float detectionRadius = 10000f;
     Collider2D playerCollider;
     Vector2 direction;
@@ -23,7 +25,7 @@ public class PointAtPlayer : MonoBehaviour
             angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
             // Apply the rotation
-            transform.rotation = Quaternion.AngleAxis(angle -45f, Vector3.forward);
+            transform.rotation = Quaternion.AngleAxis(angle + angleOffset, Vector3.forward);
         }
 
         //angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
